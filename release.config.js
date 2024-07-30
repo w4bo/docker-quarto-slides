79aad1118b3f2feaec5532f85eae2359da63f035
Matteo Francia <m.francia@unibo.it>
2024-07-30 18:49:09 +0200

    fix(ci): update the release

--- release.config.js
+++ release.config.js
@@ -2,11 +2,8 @@ const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
 input=$(git remote get-url origin)
-# Extract the part after the colon
 repo_info=\${input#*:}
-# Extract the username
 username=\${repo_info%%/*}
-# Extract the repository
 repository=\${repo_info#*/}
 repository=\${repository%.git}
 repository=\${repository#docker-}
