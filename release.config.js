348b0be7ce45438209c2dce859af440290061a25
Matteo Francia <m.francia@unibo.it>
2024-07-30 18:09:50 +0200

    ci: fixing the release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,8 @@
 const publishCmd = `
 git tag -a -f \${nextRelease.version} \${nextRelease.version} -F CHANGELOG.md
 git push --force origin \${nextRelease.version}
+docker tag ${secrets.DOCKER_USERNAME}/${secrets.DOCKER_IMAGENAME}:\${nextRelease.version} ${secrets.DOCKER_USERNAME}/${secrets.DOCKER_IMAGENAME}:latest
+docker push ${secrets.DOCKER_USERNAME}/${secrets.DOCKER_IMAGENAME}
 `;
 const config = require('semantic-release-preconfigured-conventional-commits');
 config.plugins.push(
