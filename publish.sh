2dac3705009b97d90dff5f4dc8ccc7d0685e733d
Matteo Francia <m.francia@unibo.it>
2024-07-31 15:13:44 +0200

    fix(ci): update the release

--- publish.sh
+++ publish.sh
@@ -4,10 +4,12 @@ version=$1
 username=$2
 repository=$3
 cd image
-versions=("${version}" "latest")
-for v in "${versions[@]}"; do
-docker build -t ${repository}:${v} --progress=plain . &> build.log
-docker tag ${repository} ${username}/${repository}:${v}
-docker push ${username}/${repository}:${v}
+versions=()
+for v in "${version}" "latest"
+do
+    echo $v
+    docker build -t ${repository}:${v} --progress=plain . &> build.log
+    docker tag ${repository} ${username}/${repository}:${v}
+    docker push ${username}/${repository}:${v}
 done
 cd -
