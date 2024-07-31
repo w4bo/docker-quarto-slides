3e840e7fc55eaa68d78e276b0b119f7ef6b95c56
Matteo Francia <m.francia@unibo.it>
2024-07-31 15:05:32 +0200

    fix(ci): update the release

--- publish.sh
+++ publish.sh
@@ -3,17 +3,11 @@ set -exo
 version=$1
 username=$2
 repository=$3
-# git tag -a -f ${version} ${version} -F CHANGELOG.md
-# git push --force origin ${version}
-# username=$(git config --get user.name)
-# repository=$(basename -s .git $(git config --get remote.origin.url))
-# repository=${repository%.git}
-# repository=${repository#docker-}
 cd image
 versions=("${version}" "latest")
-for version in "${versions[@]}"; do
-docker build -t ${repository}:${version} --progress=plain . &> build.log
-docker tag ${repository} ${username}/${repository}:${version}
-docker push ${username}/${repository}:${version}
+for v in "${versions[@]}"; do
+docker build -t ${repository}:${v} --progress=plain . &> build.log
+docker tag ${repository} ${username}/${repository}:${v}
+docker push ${username}/${repository}:${v}
 done
 cd -
