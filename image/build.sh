6eaa2753d0661bb8891f02858796f7b6b494b27d
Matteo Francia <m.francia@unibo.it>
2024-07-31 16:20:53 +0200

    fix(Dockerfile): add support to notebooks

--- image/build.sh
+++ image/build.sh
@@ -2,3 +2,5 @@
 set -exo
 quarto render --to revealjs
 quarto render --to pptx
+quarto render notebook.qmd --to pdf
+quarto render notebook.qmd --to revealjs
--- image/main.md
+++ image/main.md
@@ -6,7 +6,7 @@ date: 2023-12-04
 format:
   revealjs: 
     footer: "My footnote"
-bibliography: prova.bib
+bibliography: refs.bib
 ---
 
 # Main slide
